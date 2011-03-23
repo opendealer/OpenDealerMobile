25fce9334a5f99418b72895ef332cb1699b13798
OpenDealer <pablo.varando@gmail.com>
2011-03-22 19:20:23 -0500

    first run

---
+++ images/opendealer.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+var opendealer = {
+	
+};
+
+opendealer.setStorageItem = function(key,value) {
+	window.localStorage.setItem(key, value);
+}
+opendealer.getStorageItem = function(key,value) {
+	window.localStorage.getItem(key);
+}
+
